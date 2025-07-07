048949ceeda29ed2259a80741006670ee46892df
TopiasJ <TopiasJ@users.noreply.github.com>
2025-07-08 00:07:28 +0300

    feat: Transposition table for position caching

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ pub mod chromosome;
 pub mod evaluator;
 pub mod repository;
 pub mod tournament;
+pub mod transposition_table;
 pub mod visualizer;
 
 pub use alpha_beta_algorithm::*;
@@ -12,4 +13,5 @@ pub use chromosome::*;
 pub use evaluator::*;
 pub use repository::*;
 pub use tournament::*;
+pub use transposition_table::*;
 pub use visualizer::*;
